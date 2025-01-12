f04ad5117db6630b251b6142ca5ccf40fc5dc684
Wladymir Brborich <wladymir.brborich-herrera@stud.fra-uas.de>
2025-01-12 12:16:54 +0100

    Upgrade the postgresqldb connector to be flexible
    * Now it can be initialized from the main file, so we have a hyerarchy of dependencies

--- api/app/repositories/db/models.py
+++ api/app/repositories/db/models.py
@@ -1,26 +1,28 @@
-from sqlalchemy import Column, Integer, String,ForeignKey,DateTime,JSON,Rela
-from database import Base
+from app.repositories.postgres.connector import PostgreSQLDBConnector
+from sqlalchemy import JSON, Column, DateTime, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 
-class Stocks(Base):
-    __tablename__ = 'stocks'
+
+class Stock(PostgreSQLDBConnector.Base):
+    __tablename__ = "stock"
 
     id = Column(Integer, primary_key=True, nullable=False, index=True, autoincrement=True)
-    product_id = Column(Integer, ForeignKey('products.id'), nullable=False, unique=True,index=True)
+    product_id = Column(Integer, ForeignKey("products.id"), nullable=False, unique=True, index=True)
     quantity = Column(Integer, nullable=False)
     updated_at = Column(DateTime, nullable=False)
     created_at = Column(DateTime, nullable=False)
 
-    products = relationship('Products', back_populates='stocks', uselist=False)
+    products = relationship("Products", back_populates="stock", uselist=False)
 
-class Products(Base):
-    __tablename__ = 'products'
 
-    id = Column(Integer, primary_key=True, nullable=False,index=True,autoincrement=True)
+class Products(PostgreSQLDBConnector.Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, nullable=False, index=True, autoincrement=True)
     name = Column(String, nullable=False)
-    ean_code = Column(Integer, nullable=False,index=True,unique=True)
-    category_id = Column(Integer, ForeignKey('categories.id'), nullable=False)
-    supplier_id = Column(Integer, ForeignKey('suppliers.id'), nullable=False)
+    ean_code = Column(Integer, nullable=False, index=True, unique=True)
+    category_id = Column(Integer, ForeignKey("categories.id"), nullable=False)
+    supplier_id = Column(Integer, ForeignKey("suppliers.id"), nullable=False)
     description = Column(String, nullable=False)
     public_unit_price = Column(Integer, nullable=False)
     supplier_unit_price = Column(Integer, nullable=False)
@@ -30,36 +32,39 @@ class Products(Base):
     created_at = Column(DateTime, nullable=False)
     updated_at = Column(DateTime, nullable=False)
 
-    stocks = relationship('Stocks', back_populates='products')
-    categories = relationship('Categories', back_populates='products')
-    suppliers = relationship('Suppliers', back_populates='products')
-    order_details = relationship('OrderDetails',back_populates='products')
+    stock = relationship("Stock", back_populates="products")
+    categories = relationship("Categories", back_populates="products")
+    suppliers = relationship("Suppliers", back_populates="products")
+    order_details = relationship("OrderDetails", back_populates="products")
 
-class Categories(Base):
-    __tablename__ = 'categories'
 
-    id = Column(Integer, primary_key=True, nullable=False,index=True,autoincrement=True)
+class Categories(PostgreSQLDBConnector.Base):
+    __tablename__ = "categories"
+
+    id = Column(Integer, primary_key=True, nullable=False, index=True, autoincrement=True)
     name = Column(String, nullable=False)
     description = Column(String, nullable=False)
     metadata = Column(JSON, nullable=False)
 
-    products = relationship('Products', back_populates='categories', uselist=True)
+    products = relationship("Products", back_populates="categories", uselist=True)
 
-class Suppliers(Base):
-    __tablename__ = 'suppliers'
 
-    id = Column(Integer, primary_key=True, nullable=False,index=True,autoincrement=True)
+class Suppliers(PostgreSQLDBConnector.Base):
+    __tablename__ = "suppliers"
+
+    id = Column(Integer, primary_key=True, nullable=False, index=True, autoincrement=True)
     name = Column(String, nullable=False)
     address = Column(String, nullable=False)
-    phone = Column(String, nullable=False,unique=True)
-    email = Column(String, nullable=False,unique=True)
+    phone = Column(String, nullable=False, unique=True)
+    email = Column(String, nullable=False, unique=True)
 
-    products = relationship('Products', back_populates='suppliers', uselist=True)
+    products = relationship("Products", back_populates="suppliers", uselist=True)
 
-class Orders(Base):
-    __tablename__ = 'orders'
 
-    id = Column(Integer, primary_key=True, nullable=False,index=True,autoincrement=True)
+class Orders(PostgreSQLDBConnector.Base):
+    __tablename__ = "orders"
+
+    id = Column(Integer, primary_key=True, nullable=False, index=True, autoincrement=True)
     order_total = Column(Integer, nullable=False)
     customer_name = Column(String, nullable=False)
     customer_shipping_info = Column(JSON, nullable=False)
@@ -70,17 +75,18 @@ class Orders(Base):
     created_at = Column(DateTime, nullable=False)
     updated_at = Column(DateTime, nullable=False)
 
-    order_details = relationship('OrderDetails', back_populates='orders', uselist=True)
+    order_details = relationship("OrderDetails", back_populates="orders", uselist=True)
 
-class OrderDetails(Base):
-    __tablename__ = 'order_details'
 
-    id = Column(Integer, primary_key=True, nullable=False,index=True,autoincrement=True)
-    product_id = Column(Integer, ForeignKey('products.id'), nullable=False, index=True)
-    order_id = Column(Integer, ForeignKey('orders.id'), nullable=False)
+class OrderDetails(PostgreSQLDBConnector.Base):
+    __tablename__ = "order_details"
+
+    id = Column(Integer, primary_key=True, nullable=False, index=True, autoincrement=True)
+    product_id = Column(Integer, ForeignKey("products.id"), nullable=False, index=True)
+    order_id = Column(Integer, ForeignKey("orders.id"), nullable=False)
     product_price = Column(Integer, nullable=False)
     created_at = Column(DateTime, nullable=False)
     updated_at = Column(DateTime, nullable=False)
 
-    orders = relationship('orders', back_populates='order_details')
-    products = relationship('Products', back_populates='order_details')
+    orders = relationship("orders", back_populates="order_details")
+    products = relationship("Products", back_populates="order_details")
---
+++ api/app/repositories/postgres/connector.py
@@ -0,0 +1,26 @@
+from typing import Any, AsyncGenerator, Optional
+
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
+
+
+class PostgreSQLDBConnector:
+    engine: Optional[AsyncEngine] = None
+    sessionmaker: Any = None
+
+    class Base(DeclarativeBase):
+        pass
+
+    @classmethod
+    def init_db(cls, connection_str: str) -> None:
+        if not cls.engine:
+            cls.engine = create_async_engine(connection_str)
+
+        if not cls.sessionmaker:
+            cls.sessionmaker = async_sessionmaker(cls.engine, expire_on_commit=False)
+
+    @classmethod
+    async def get_session(cls) -> AsyncGenerator[AsyncSession]:
+        session = cls.sessionmaker()
+        async with session as session:
+            yield session
