a8f8c68717d1abe0b8683165b131f665ea4d03e7
Wladymir Brborich <wladymir.brborich-herrera@stud.fra-uas.de>
2025-01-12 12:17:21 +0100

    Update the environment files and manager
    * Add examples to avoid confusion

--- api/app/config/env_manager.py
+++ api/app/config/env_manager.py
@@ -5,7 +5,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    POSTGRESQL_DB_URL: str
+    AZURE_DATABASE_URL: str
 
     model_config = SettingsConfigDict(env_file=".env")
 
