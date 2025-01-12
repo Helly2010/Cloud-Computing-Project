8451e7d8ae3279ea917eb7117c5e5bf510d60d74
Wladymir Brborich <wladymir.brborich-herrera@stud.fra-uas.de>
2025-01-12 12:17:32 +0100

    Initialize the database in the main file

--- api/app/main.py
+++ api/app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
+from .repositories.postgres.connector import PostgreSQLDBConnector
 from .routes import products_router
+from .config.env_manager import get_settings
+
+EnvManager = get_settings()
 
 
 app = FastAPI()
-
+PostgreSQLDBConnector.init_db(EnvManager.AZURE_DATABASE_URL)
 
 app.include_router(products_router)
