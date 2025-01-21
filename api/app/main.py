6c1bc05ff7bf1a52f6449ee8a786e18a33760920
Wladymir Brborich <wladymir.brborich-herrera@stud.fra-uas.de>
2025-01-21 13:07:58 +0100

    Initialize the email connector

--- api/app/main.py
+++ api/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .repositories.postgres.connector import PostgreSQLDBConnector
+from .repositories.email.connector import EmailConnector
 from .routes import products_router, categories_router, orders_router, suppliers_router, stock_router
 from .config.env_manager import get_settings
 
@@ -9,6 +10,9 @@ EnvManager = get_settings()
 
 app = FastAPI()
 PostgreSQLDBConnector.init_db(EnvManager.get_db_url())
+EmailConnector.init_connector(
+    EnvManager.MAIL_USERNAME, EnvManager.MAIL_PASSWORD, EnvManager.MAIL_FROM, EnvManager.MAIL_PORT, EnvManager.MAIL_SERVER
+)
 
 app.add_middleware(
     CORSMiddleware,
