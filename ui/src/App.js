9fea1723772bc214d68e961cc0b762344d2389f0
Wladymir Brborich <wladymir.brborich-herrera@stud.fra-uas.de>
2025-02-04 12:41:54 +0100

    Formatting

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,18 +1,18 @@
-import { loadStripe } from '@stripe/stripe-js';
-import './App.css';
-import CheckoutForm from './components/CheckoutForm';
-import { Elements } from '@stripe/react-stripe-js';
-import { BrowserRouter, Route, Routes} from 'react-router-dom';
-import Header from './components/Header';
-import Home from './components/Home';
-import Cart from './components/Cart';
-import { ToastContainer } from 'react-toastify';
-import ProductList from "./components/ProductList";
+import { loadStripe } from "@stripe/stripe-js";
+import "./App.css";
+import CheckoutForm from "./components/CheckoutForm";
+import { Elements } from "@stripe/react-stripe-js";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
+import { ToastContainer } from "react-toastify";
 import ProductDetail from "./components/ProductDetail";
 import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
-
-const stripePromise = loadStripe('pk_test_51MwPQuSBD8MtMZAoDOk33CGs935GKRdxMeR3HN4Rro4g8HIuIPOMfDRLHoEYWPFPHIpK0RfN5Gc9zbKOhqcMzMPn00z8zgZCFw');
+const stripePromise = loadStripe(
+  "pk_test_51MwPQuSBD8MtMZAoDOk33CGs935GKRdxMeR3HN4Rro4g8HIuIPOMfDRLHoEYWPFPHIpK0RfN5Gc9zbKOhqcMzMPn00z8zgZCFw"
+);
 
 const App = () => {
   return (
@@ -36,9 +36,6 @@ const App = () => {
                 <Elements stripe={stripePromise}>
                   <CheckoutForm />
                 </Elements>
-
-                
-                
               </div>
             }
           />
