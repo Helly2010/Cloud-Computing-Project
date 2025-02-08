12b4b1ddaffc3d1eecf1d7743d05106ff86fa243
Wladymir Brborich <wladymir.brborich-herrera@stud.fra-uas.de>
2025-02-08 11:27:14 +0100

    Improve error handling for APIs

--- ui/src/services/api/categories.js
+++ ui/src/services/api/categories.js
@@ -4,19 +4,28 @@ import EnvManager from "../../config/envManager";
 const BASE_URL = EnvManager.BACKEND_URL;
 
 const fetchCategories = async () => {
-  const response = await fetch(`${BASE_URL}/categories/`);
+  try {
+    const response = await fetch(`${BASE_URL}/categories/`);
 
-  if (!response.ok) {
+    if (!response.ok) {
+      toast.error("Error while loading categories", {
+        position: "top-left",
+        autoClose: 1500,
+        closeOnClick: true,
+      });
+      return [];
+    }
+    const categories = await response.json();
+    return categories;
+  } catch (error) {
     toast.error("Error while loading categories", {
       position: "top-left",
       autoClose: 1500,
       closeOnClick: true,
     });
-    return;
+    console.log(error);
+    return [];
   }
-
-  const categories = await response.json();
-  return categories;
 };
 
 export { fetchCategories };
--- ui/src/services/api/products.js
+++ ui/src/services/api/products.js
@@ -4,35 +4,55 @@ import EnvManager from "../../config/envManager";
 const BASE_URL = EnvManager.BACKEND_URL;
 
 const fetchProducts = async () => {
-  const response = await fetch(`${BASE_URL}/products/`);
+  try {
+    const response = await fetch(`${BASE_URL}/products/`);
 
-  if (!response.ok) {
-    toast.error("Error while loading products", {
+    if (!response.ok) {
+      toast.error("Error while loading products", {
+        position: "top-left",
+        autoClose: 1500,
+        closeOnClick: true,
+      });
+      return;
+    }
+
+    const products = await response.json();
+    return products;
+  } catch (error) {
+    toast.error("Error while loading categories", {
       position: "top-left",
       autoClose: 1500,
       closeOnClick: true,
     });
+    console.log(error);
     return;
   }
-
-  const products = await response.json();
-  return products;
 };
 
 const fetchProduct = async (id) => {
-  const response = await fetch(`${BASE_URL}/products/${id}`);
+  try {
+    const response = await fetch(`${BASE_URL}/products/${id}`);
 
-  if (!response.ok) {
-    toast.error("Error while loading products", {
+    if (!response.ok) {
+      toast.error("Error while loading products", {
+        position: "top-left",
+        autoClose: 1500,
+        closeOnClick: true,
+      });
+      return [];
+    }
+
+    const product = await response.json();
+    return product;
+  } catch (error) {
+    toast.error("Error while loading categories", {
       position: "top-left",
       autoClose: 1500,
       closeOnClick: true,
     });
-    return;
+    console.log(error);
+    return [];
   }
-
-  const product = await response.json();
-  return product;
 };
 
 export { fetchProducts, fetchProduct };
