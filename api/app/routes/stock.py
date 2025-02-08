e4c7d77c1281c171f2d0485123b3af1705ee4099
Wladymir Brborich <wladymir.brborich-herrera@stud.fra-uas.de>
2025-02-09 00:43:55 +0100

    remove expire_all and unnecessary comments

--- api/app/routes/stock.py
+++ api/app/routes/stock.py
@@ -12,18 +12,16 @@ router = APIRouter()
 
 @router.get("/stock/{product_id}", response_model=dict)
 async def get_stock(product_id: int, db: AsyncSession = Depends(PostgreSQLDBConnector.get_session)):
-    # Query to find the product based on product_id
     db_product = await db.get(Product, product_id)
     if not db_product:
         raise HTTPException(status_code=404, detail="Product not found")
 
-    # Query to get the stock related to the product
     db_stock = await db.get(Stock, db_product.stock_id)
     if not db_stock:
         raise HTTPException(status_code=404, detail="Stock not found for this product")
 
-    # Return stock quantity in a dictionary format
     return {"product_id": product_id, "quantity": db_stock.quantity}
+
 
 @router.patch("/stock/{product_id}", response_model=StockSerializer)
 async def update_stock(
@@ -42,8 +40,5 @@ async def update_stock(
 
         await db.commit()
         await db.refresh(db_stock)
-
-        # Expire the session state to ensure up-to-date data on subsequent requests
-        await db.expire_all()
 
     return db_stock
