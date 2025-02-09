01a76161ce862adf01e9749ea80ee37918960d73
Wladymir Brborich <wladymir.brborich-herrera@stud.fra-uas.de>
2025-02-09 22:30:23 +0100

    Fix manually configured files

--- infra/staticwebapp/main.tf
+++ infra/staticwebapp/main.tf
@@ -10,4 +10,7 @@ resource "azurerm_static_web_app" "frontend" {
   name                = "lowtechgmbh-webshop"
   resource_group_name = azurerm_resource_group.web_app_rg.name
   location            = azurerm_resource_group.web_app_rg.location
+  repository_branch   = "main"
+  repository_url      = "https://github.com/Wason1797/Cloud-Computing-Project"
+  repository_token    = "*"
 }
