908f60b91980943328f0b7ae1c68053b03505a87
Wladymir Brborich <wladymir.brborich-herrera@stud.fra-uas.de>
2025-02-09 19:25:31 +0100

    Improve resource creation with dependencies
    * To ensure critical resources are created first. For some reason it did not work as expected without this configurations.

--- infra/main.tf
+++ infra/main.tf
@@ -45,10 +45,12 @@ resource "azurerm_subnet" "vn_subnet" {
       ]
     }
   }
+  depends_on = [azurerm_virtual_network.vn ]
 }
 resource "azurerm_private_dns_zone" "private_dns_zone" {
   name                = "lowtechgmbh.postgres.database.azure.com"
   resource_group_name = azurerm_resource_group.main_rg.name
+  depends_on = [ azurerm_resource_group.main_rg ]
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "vnet_link" {
